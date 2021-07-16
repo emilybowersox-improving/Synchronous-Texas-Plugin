aafd06633524e5445dd01493e5648aca40e65efc
Emily Bowersox <emily.bowersox@improving.com>
2021-07-16 14:54:17 -0500

    plugin works- returns different error messages for null state or non-texas state

--- TexasPlugin.cs
+++ TexasPlugin.cs
@@ -43,24 +43,49 @@ namespace TexasPlugin
                     //check in entity is account
                     //check is state is null, throw new InvalidPluginExecutionException("You must include a state to save an account")
 
-                    tracingService.Trace("beginning Try- {0}", DateTime.Now.ToLongTimeString());
+                    tracingService.Trace("beginning state validation - {0}", DateTime.Now.ToLongTimeString());
 
-                    if(context.MessageName == "Create")
+                    if (context.MessageName == "Create")
                     {
                         tracingService.Trace("context.MessageName = Create - {0}", DateTime.Now.ToLongTimeString());
-                        string state = entity["address1_stateorprovince"].ToString().ToLower();
+                        
 
-                        if (state == "texas" || state == "tx")
+                        if(!entity.Attributes.ContainsKey("address1_stateorprovince"))
                         {
+                            tracingService.Trace("State was empty - {0}", DateTime.Now.ToLongTimeString());
+                            throw new InvalidPluginExecutionException("You must include a State/Province to save an Account");
+                        }
+
+                        string state = entity["address1_stateorprovince"].ToString().ToLower();
+                        if (state == "texas" || state == "tx")
+                        { 
                             tracingService.Trace("state is texas - {0}", DateTime.Now.ToLongTimeString());
-                            return;
-                            } else
-                            {
-                                throw new InvalidPluginExecutionException("You can only save Accounts with the state of Texas or TX");
-                            }
-                         }
+                        } else
+                        {
+                            tracingService.Trace("User tried to input state:{1} - {0}", DateTime.Now.ToLongTimeString(), entity["address1_stateorprovince"].ToString());
+                            throw new InvalidPluginExecutionException("You can only save Accounts that are located in Texas/TX");
+                        }
+
+                  /*      if (!entity.TryGetAttributeValue<string>(entity["address1_stateorprovince"].ToString(), out string result))
+                        {
+                            tracingService.Trace("State was empty - {0}", DateTime.Now.ToLongTimeString());
+                            throw new InvalidPluginExecutionException("You must include a State/Province to save an Account");
+                        }*/
+            /*            if (state == "texas" || state == "tx")
+                        {
+                      
+                        }
+                        if (state != "texas" && state != "tx")
+                        {
+                            tracingService.Trace("User tried to input state:{1} - {0}", DateTime.Now.ToLongTimeString(), entity["address1_stateorprovince"].ToString());
+                            throw new InvalidPluginExecutionException("You can only save Accounts that are located in Texas/TX");
+                        }*/
 
                     }
+                 
+
+
+                }
 
                 catch (FaultException<OrganizationServiceFault> ex)
                 {
